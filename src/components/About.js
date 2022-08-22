c6904d7031d654ce137365cdae47bfc05511cac8
plsnotjoe <96671050+plsnotjoe@users.noreply.github.com>
2022-08-22 23:39:05 +0100

    Initial commit

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import { useNavigate } from 'react-router-dom'
+import Button from "./Button";
+
+const About = () => {
+    const navigate = useNavigate()
+
+    return (
+        <div>
+            <h4>Version 0.0.1</h4>
+            <Button onClick={() => {navigate(-1)}} text='Back' />
+        </div>
+    )
+}
+
+export default About
